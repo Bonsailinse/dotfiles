7d34554ca321982ad8e2c7d530449972468e47ec
Bonsailinse <mail@bonsailinse.net>
2020-04-30 21:48:26 +0200

    feature: Add dot binary for maintaining dotfiles; fix paths
    bin/dot handles the maintenance of the dotfile installation through
    installing and updating components. Paths in install.sh are updated.

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 #
 # Run all dotfiles installers.
 
-DOTFILES_ROOT=$(pwd -P)
+DOTFILES="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
+export DOTFILES
 
 set -e
 
@@ -42,7 +43,7 @@ setup_gitconfig () {
     user ' - What is your GitHub author email?'
     read -e git_authoremail
 
-    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.local.symlink.example > git/gitconfig.local.symlink
+    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" "$DOTFILES"/git/gitconfig.local.symlink.example > "$DOTFILES"/git/gitconfig.local.symlink
 
     success 'gitconfig'
   fi
@@ -56,18 +57,14 @@ link_file () {
 
   if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
   then
-
     if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
     then
-
       local currentSrc
       currentSrc="$(readlink $dst)"
 
       if [ "$currentSrc" == "$src" ]
       then
-
         skip=true;
-
       else
 
         user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
@@ -129,7 +126,7 @@ install_dotfiles () {
 
   local overwrite_all=false backup_all=false skip_all=false
 
-  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
+  for src in $(find -H "$DOTFILES" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
   do
     dst="$HOME/.$(basename "${src%.*}")"
     link_file "$src" "$dst"
